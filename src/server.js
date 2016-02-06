655a7a1d72d74a570f2da5bc7110c16d46c44a9d
Matt Dotson <matt@dotsons.org>
2016-02-05 17:55:17 -0800

    hello world

---
+++ src/server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var port = process.env.port | 1337;
+var app = express();
+
+app.get('/', function(req, res){
+	res.write('bizglue/bizglue');
+	res.end();
+});
+
+app.listen(port, function(){
+	console.log('Example app listening on port' + port);
+});
